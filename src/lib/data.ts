e93d9db5b223903e6a20e1b363de5964eeb8478c
arrlancores <arrlancore@gmail.com>
2023-05-08 08:51:39 +0700

    chore: add tamu iis

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -13,6 +13,47 @@ export const dataGuest = {
   ari: 'Ari & Keluarga',
   dio: 'Dio & Keluarga',
   default: 'Bapak/Ibu/Saudara/i',
+  // tamu iis
+  'nur-hidayati': 'Nur Hidayati',
+  'ukmd-azzamul-iffah': "Ukmd 'Azzamul 'Iffah",
+  'elma-sintia': 'Elma Sintia',
+  esi: 'Esi',
+  anita: 'Anita',
+  putri: 'Putri & Suami',
+  asni: 'Asni & Suami',
+  sarwedi: 'Sarwedi & Istri',
+  gilang: 'Gilang',
+  hadi: 'Hadi Subeno',
+  zikra: 'Zikra',
+  'grup-mutiijah': ' Grup Mutiijah',
 } as const;
 
 export type GustKeys = keyof typeof dataGuest;
+
+// const generateLink: () => {
+//   Object.keys(dataGuest).map(key => console.info(`${appDomain}/undang/${key}`))
+// }
+
+// "https://arlankenny.site/undang/nur-hidayati"
+
+// "https://arlankenny.site/undang/ukmd-azzamul-iffah"
+
+// "https://arlankenny.site/undang/elma-sintia"
+
+// "https://arlankenny.site/undang/esi"
+
+// "https://arlankenny.site/undang/anita"
+
+// "https://arlankenny.site/undang/putri"
+
+// "https://arlankenny.site/undang/asni"
+
+// "https://arlankenny.site/undang/sarwedi"
+
+// "https://arlankenny.site/undang/gilang"
+
+// "https://arlankenny.site/undang/hadi"
+
+// "https://arlankenny.site/undang/zikra"
+
+// "https://arlankenny.site/undang/grup-mutiijah"
